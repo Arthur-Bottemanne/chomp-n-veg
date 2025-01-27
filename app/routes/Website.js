2b9cc4e8f5998fd1e6268c6eee55071813ded9f4
Arthur-Bottemanne <arthurbottemanne@gmail.com>
2025-01-27 08:26:54 +0100

    feat: add login view

--- app/routes/Website.js
+++ app/routes/Website.js
@@ -7,6 +7,10 @@ router.get("/", (req, res) => {
     res.send("Welcome to the website!");
 });
 
+router.get("/login", (req, res) => {
+    res.render("login");
+});
+
 router.get("/register", (req, res) => {
     res.render("register");
 });
