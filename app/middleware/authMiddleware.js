c64b39c61b947c9ce2b17a19381e28d6b7b23391
Arthur-Bottemanne <arthurbottemanne@gmail.com>
2025-01-30 08:47:01 +0100

    fix: modify middleware to correspond with token implementation

--- app/middleware/authMiddleware.js
+++ app/middleware/authMiddleware.js
@@ -1,13 +1,11 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-    const authHeader = req.headers.authorization;
+    const token = req.cookies.token;
 
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    if (!token) {
         return res.status(401).json({ message: "Unauthorized" });
     }
-
-    const token = authHeader.split(" ")[1];
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
